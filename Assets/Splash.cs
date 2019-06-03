10bbab1e96d6ba8b15d55ad0849c264ef586bd60
Jay Peterson <jaypeterson47@live.com>
2019-06-03 16:16:52 -0500

    Added terrain Added skybox Added player ship Added splash screen

---
+++ Assets/Splash.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+	void Start ()
+	{
+		Invoke("LoadGame", 3);
+	}
+
+	void LoadGame()
+	{
+		SceneManager.LoadScene("Level 1");
+	}
+}
